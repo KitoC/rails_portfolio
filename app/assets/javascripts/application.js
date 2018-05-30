9bc8a48d40f8f9b85ef03542d09ebb40d775c58a
Kito Clark <kitoclark1989@gmail.com>
2018-05-30 14:52:41 +1000

    More stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -46,13 +46,19 @@ function checkTime(i) {
 
 let homeClick = false;
 
+
 function hintBox() {
   setTimeout(function () {
-    if (!homeClick) {
-      $('.info-box').addClass('info-b-active')
+
+    if ($(document).width() >= 600) {
+      if (!homeClick) {
+        $('.info-box').addClass('info-b-active')
+      }
     }
+
   }, 3000)
 }
+
 
 // if ($(document).width() >= 600) {
 //   $(".project-scr").addClass('not-phone');
@@ -73,7 +79,9 @@ $(document).ready(function () {
       setTimeout(function () {
         $(".home-screen").addClass('active');
         $(".loading-screen").removeClass('active');
-        $(".headGrid").removeClass('batt_hide')
+        if (showLoading) {
+          $(".headGrid").removeClass('hide');
+        }
 
       }, 14000);
     } else {
@@ -85,8 +93,14 @@ $(document).ready(function () {
       $(".contact-screen").removeClass('active')
       $(".project-screen").removeClass('active')
       $(".headGrid").removeClass('batt_black')
-      $(".headGrid").removeClass('batt_hide')
+      $(".headGrid").removeClass('hide')
       $(".info-box").removeClass('info-b-active')
+      $(".projects").removeClass('projects-active')
+      $(".project-tiles").removeClass('home-projects-active')
+      $(".full-bg-blur").removeClass('bg-blur-active')
+      $(".tile-title").removeClass('tt-show')
+      $(".h-b-title").removeClass('hide')
+      $('.project-images, .project-text').children().remove()
 
       if (orient === false) {
         $('.center').addClass('portraitC');
@@ -112,15 +126,26 @@ $(document).ready(function () {
 });
 
 // script for project button
-function projectOverlay () {
-  $(".polaroid-r, .polaroid-l").on('click', function () {
+let project_index = null
+
+function projectOverlay() {
+  $(".project-tiles").on('click', function () {
+    project_index = project_json[$(this).data('id')]
+    console.log(project_index)
+    // console.log($(this).class())
+
     $(".project-screen").addClass('active')
+    $('.project-images').append(lge_img(project_index))
+    $('.project-text').append(project_text(project_index))
+    $(".headGrid").addClass('hide')
   });
 };
 
 $(document).ready(function () {
-  $(".project-overlay").click(function () {
+  $(".project-close-b").click(function () {
     $(".project-screen").removeClass('active')
+    $('.project-images, .project-text').children().remove()
+    $(".headGrid").addClass('hide')
   });
 });
 
@@ -153,6 +178,21 @@ $(document).ready(function () {
   });
 });
 
+// script for project tile button
+$(document).ready(function () {
+  $(".project-b-overlay, .full-bg-blur, .tt").click(function () {
+    $(".projects").toggleClass('projects-active')
+    $(".project-tiles").toggleClass('home-projects-active')
+    $(".full-bg-blur").toggleClass('bg-blur-active')
+    $(".headGrid").toggleClass('hide')
+    $(".tile-title").toggleClass('tt-show')
+    $(".h-b-title").toggleClass('hide')
+
+
+  });
+});
+
+
 // script for orientation button
 orient = true;
 $(document).ready(function () {
@@ -160,7 +200,7 @@ $(document).ready(function () {
     if ((orient)) {
       $('.center').addClass('landscapeC');
       $('.screen').addClass('landscapeS');
-      $(".headGrid").addClass('batt_hide')
+      $(".headGrid").addClass('hide')
       $('.polaroid-container').addClass('add-flex');
       $('.center').removeClass('portraitC');
       $('.screen').removeClass('portraitS');
@@ -168,7 +208,7 @@ $(document).ready(function () {
     } else {
       $('.center').removeClass('landscapeC');
       $('.screen').removeClass('landscapeS');
-      $(".headGrid").removeClass('batt_hide')
+      $(".headGrid").removeClass('hide')
       $('.polaroid-container').removeClass('add-flex');
       $('.center').addClass('portraitC');
       $('.screen').addClass('portraitS');
@@ -177,22 +217,147 @@ $(document).ready(function () {
   });
 });
 
+// script for image toggle
+let image_pos = 2
+$(document).ready(function () {
+  $('.i-one').click(function () {
+    image_pos = 1
+  })
+  $('.i-two').click(function () {
+    image_pos = 2
+  })
+  $('.i-three').click(function () {
+    image_pos = 3
+  })
+
+  if (image_pos == 1) {
+    $('.project-images').addClass('left-f')
+    $('.project-images').removeClass('right-f')
+    $('.project-images').removeClass('middle-f')
+  } else if (image_pos == 2) {
+    $('.project-images').addClass('middle-f')
+    $('.project-images').removeClass('right-f')
+    $('.project-images').removeClass('left-f')
+  } else if (image_pos == 3) {
+    $('.project-images').addClass('right-f')
+    $('.project-images').removeClass('middle-f')
+    $('.project-images').removeClass('left-f')
+  }
+});
+
 
 const project_json = [{
-  title: "Ruby Zombie Run",
-  image: 'zmedbattle.png'
+  id: '0',
+  title: 'RbZombies',
+  image: ['zmedbattle.png', 'ztallmap.png', 'zshortcc.png'],
+  techStack: ['ruby-logo.png'],
+  date: 'February 2018',
+  description: 'Foo',
+  link: 'https://github.com/KitoC/coderAcademyAsssignment-1',
+  linkTag: 'Checkout the GitHub repo.',
+  color: '#272822',
+  tileImage: 'zomb-icon.png'
+}, {
+  id: '1',
+  title: 'ShareSpace',
+  image: ['ss-1.png'],
+  techStack: ['rails-icon.png', 'Heroku-logo.png'],
+  date: 'February 2018',
+  description: '',
+  link: 'https://stark-anchorage-29476.herokuapp.com/',
+  linkTag: 'Checkout the live app.',
+  color: 'blue',
+  tileImage: ''
+}, {
+  id: '2',
+  title: 'ToDo List',
+  image: ['KC101.png'],
+  techStack: ['ruby-logo.png'],
+  date: 'February 2018',
+  description: '',
+  link: '',
+  linkTag: '',
+  color: 'blue',
+  tileImage: ''
 }]
+
+
 
 function projectsDisplay() {
   let array = project_json
   for (project of array) {
-    $('.polaroids-t').append(`<div class="polaroid-l">
-      <div class="polaroid-pin"></div>
-      <div class = "polaroid-img"
-      style = "background-image: url('assets/${project.image}');" >
-      </div>
-      <p class="texta">${project.title}</p>
-    </div>`)
-    projectOverlay()
+    $('.home-projects').append(polaroid_var(project))
   }
+  projectOverlay()
+}
+
+// Polaroid template
+function polaroid_var(object, position) {
+  let stack = ''
+  for (ts of object.techStack) {
+    stack += `<div class="ts-image"
+      style="background-image: url('assets/${ts}');">
+      </div>`
+  }
+  return `
+  <div data-id="${object.id}"
+  class="button-tile project-tiles"
+  style="background-image: url('assets/${object.tileImage}');background-color:${object.color};" >
+  <p>${object.title}</p>
+  </div>
+`
+  // <div class="polaroid-box">
+  //   <div data-id="${object.id}"
+  //   class="polaroid"
+  //   style="transform: rotate(${position}deg)">
+  //     <div class="polaroid-pin"></div>
+  //     <div class="polaroid-img"
+  //     style="background-image: url('assets/${object.image[0]}');">
+  //     </div>
+  //     <p class="texta">${object.title}</p>
+  //   </div><div class="tech-stack">${stack}</div>
+  //   </div>
+}
+
+// Large image template
+function lge_img(object, count) {
+  let image_count = count
+  let images = ""
+  let i = 0
+  for (image of object.image) {
+    images += `
+    <div data-id="${object.id}"
+  class="lge-project-frame img-${i}">
+  <div class="lge-project-img"
+  style="background-image: url('assets/${object.image[i]}');">
+  <h3 class="texta">${object.title}</h3>
+  </div>
+  </div>`
+    i++
+  }
+  return images
+}
+
+
+function project_text(project) {
+  let text = `
+  <a class="project-link" href="${project.link}">${project.linkTag}</a>
+    <p class='project-description'>${project.description}</p>`
+  return text
+}
+
+function specificRandom() {
+  let set1 = Math.floor((Math.random() * 7) + 3.5);
+  let set2 = Math.floor((Math.random() * -7) + -3.5);
+  let picker = true
+
+  if (picker) {
+    picker = false
+    return set1
+  } else {
+    picker = true
+    return set2
+  }
+
+
 }
